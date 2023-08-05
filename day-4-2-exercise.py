f60f26018ddd1b34b4144b424543aadf7461c166
Anyoh98 <fonachuanyoh@yahoo.com>
2023-08-05 12:25:47 +0100

    updated it to select people at random

--- day-4-2-exercise.py
+++ day-4-2-exercise.py
@@ -1,9 +1,12 @@
 # Split string method
+import random
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
+# Write your code below this line 👇
 print(names)
-
-
+num_items = len(names)
+random_choice = random.randint(0, num_items - 1)
+person_to_pay = names[random_choice]
+print(person_to_pay)
