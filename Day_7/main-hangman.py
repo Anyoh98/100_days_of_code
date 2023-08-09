cb9176bfb747de5eb7ab4078d78424cbc1cc0f4e
Anyoh98 <fonachuanyoh@yahoo.com>
2023-08-09 16:09:27 +0100

    Hangman images now print properly

--- Day_7/main-hangman.py
+++ Day_7/main-hangman.py
@@ -1,86 +1,36 @@
+from Hangman_art import logo
 import random
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-word_list = ["aardvark", "baboon", "camel"]
+
+from Hangman_art import stages
+from Hangman_words import word_list
 random_word = random.choice(word_list)
-
-print("Pssstt... The random word chosen is {""}".format(random_word))
-
-display = []
-i = 0
-for letter in random_word:
-    display.insert(i, "_")
-    i += 1
-print(display)
 
 game_over = False
 lives_left = 6
 
+print(logo)
+
+print("Pssstt... The random word chosen is '{}'".format(random_word))
+
+display = []
+for letter in range(len(random_word)):
+    display += "_"
+print(display)
+
 while not game_over and lives_left > 0:
     guess = input("Guess a letter: ").lower()
-
+    if guess in display:
+        print(f"You've already guessed '{guess}'")
     for position in range(len(random_word)):
         letter = random_word[position]
         if letter == guess:
             display.pop(position)
             display.insert(position, letter)
     if guess not in random_word:
-        print("Please ill be hung, character wasnt correct😭😭😭!")
+        print("Oh no!! '{}' is not in the word, You lose a life😩".format(guess))
         lives_left -= 1
+        print(stages[lives_left])
+
     print(display)
 
     if "_" not in display:
